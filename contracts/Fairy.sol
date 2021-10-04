3d638302ea74975b9bcbe5e1677e26d23b4120a1
george-shammar <george.gbenle@outlook.com>
2021-10-04 06:29:44 +0000

    Create contract

--- contracts/Fairy.sol
+++ contracts/Fairy.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Fairy {
-    
+contract Fairy is ERC20{
+
+    constructor() ERC20('Fairy', 'FRY'){
+
+    }
 }
