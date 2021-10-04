d3c9a7b729a4e1a0e3ee419b860d404b286c2b39
george-shammar <george.gbenle@outlook.com>
2021-10-04 08:06:23 +0000

    Override totalSupply

--- contracts/Fairy.sol
+++ contracts/Fairy.sol
@@ -55,4 +55,8 @@ contract Fairy is ERC20{
         require(amount <= SafeMath.div(SafeMath.mul(_totalSupply, mintCap), 100), "FRY::mint: exceeded mint cap");
         _mint(msg.sender, amount);
     }
+
+    function totalSupply() public view virtual override returns (uint256) {
+        return _totalSupply;
+    }
 }
